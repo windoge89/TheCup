691d8cbb9cdd90751d198604cb7fd006f2f5f777
TomCassey42 <51101688+TomCassey42@users.noreply.github.com>
2019-05-29 15:07:33 -0700

    Push

--- Cup/src/theCup/MyFrame.java
+++ Cup/src/theCup/MyFrame.java
@@ -1,16 +1,59 @@
 package theCup;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.*;
+import javax.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+
+import org.opencv.core.*;
+import org.opencv.core.Core.*;
+
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.*;
+import org.opencv.objdetect.*;
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame{
     private JPanel contentPane;
-    DemoPipeline CV = new DemoPipeline();
+    double startTime = System.currentTimeMillis();
+    NeonPipeline CV = new NeonPipeline();
     public static void main(String[] args) {
+    	
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
@@ -24,6 +67,7 @@ public class MyFrame extends JFrame {
     }
 
     public MyFrame() {
+    	
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(0, 0, 1280, 720);
         contentPane = new JPanel();
@@ -33,19 +77,43 @@ public class MyFrame extends JFrame {
 
         new MyThread().start();
     }
-
+    
     VideoCap videoCap = new VideoCap(0);
-
+    
     public void paint(Graphics g){
         g = contentPane.getGraphics();
         videoCap.read();
-        CV.process(videoCap.getCapturedMat());
-        g.drawImage(videoCap.getImage(CV.hsvThresholdOutput()), 0, 0, this);
+        Mat image = videoCap.getCapturedMat();
+        CV.process(image);
+        Point center = toolBox.findCentroid(CV.hsvThresholdOutput());
+        if(Double.toString(center.x) != "NaN") {
+        CV.addPoint((Point)center);
+        }
+        Mat output = CV.hsvThresholdOutput();
+        System.out.println(1000*CV.getPointsSize()/(System.currentTimeMillis()-this.startTime));
+        try {
+        	if(CV.getPointsSize() < 60) {
+			g.drawImage(toolBox.drawCircle(image, center, new Scalar(200, 0, 0)), 0, 0, this);
+        	}else {
+        	/*int x = 1;
+        	while(x<6) {
+        		image = toolBox.image2Mat(toolBox.drawCircle(image, CV.getPoint(CV.getPointsSize()-x), new Scalar(200, 0, 0)));
+        		x++;
+        	}
+        	Point prediction = new Point();
+        	*/		
+        	g.drawImage(toolBox.drawCurve(image, CV.points),0,0,this);
+        	}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
     class MyThread extends Thread{
+    	
         @Override
         public void run() {
+        	
             for (;;){
                 repaint();
                 try { Thread.sleep(30);
---
+++ Cup/src/theCup/toolBox.java
@@ -0,0 +1,174 @@
+package theCup;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+
+public class toolBox {
+	public static Mat bufferedImage2Mat(BufferedImage in)
+    {
+          Mat out;
+          byte[] data;
+          int r, g, b;
+          int height = in.getHeight();
+          int width = in.getWidth();
+          if(in.getType() == BufferedImage.TYPE_INT_RGB || in.getType() == BufferedImage.TYPE_INT_ARGB)
+          {
+              out = new Mat(height, width, CvType.CV_8UC3);
+              data = new byte[height * width * (int)out.elemSize()];
+              int[] dataBuff = in.getRGB(0, 0, width, height, null, 0, width);
+              for(int i = 0; i < dataBuff.length; i++)
+              {
+                  data[i*3 + 2] = (byte) ((dataBuff[i] >> 16) & 0xFF);
+                  data[i*3 + 1] = (byte) ((dataBuff[i] >> 8) & 0xFF);
+                  data[i*3] = (byte) ((dataBuff[i] >> 0) & 0xFF);
+              }
+          }
+          else
+          {
+              out = new Mat(height, width, CvType.CV_8UC1);
+              data = new byte[height * width * (int)out.elemSize()];
+              int[] dataBuff = in.getRGB(0, 0, width, height, null, 0, width);
+              for(int i = 0; i < dataBuff.length; i++)
+              {
+                r = (byte) ((dataBuff[i] >> 16) & 0xFF);
+                g = (byte) ((dataBuff[i] >> 8) & 0xFF);
+                b = (byte) ((dataBuff[i] >> 0) & 0xFF);
+                data[i] = (byte)((0.21 * r) + (0.71 * g) + (0.07 * b)); //luminosity
+              }
+           }
+           out.put(0, 0, data);
+           return out;
+     }
+    
+    public static Mat image2Mat( Image image) {
+
+    	
+    	BufferedImage bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(image, 0, 0, null);
+        bGr.dispose();
+
+        return bufferedImage2Mat( bimage);
+
+    }
+    
+    public static Point quadraticInterpolation(Point p1, Point p2, Point p3) {
+    	
+    	Point p4 = new Point();
+    	p4.x = p1.x + 2*(p1.x - p3.x);
+    	p4.y = p3.y*(p4.x - p1.x)*(p4.x-p2.x)/((p3.x - p1.x)*(p3.x-p2.x))+
+    			p2.y*(p4.x - p1.x)*(p4.x-p3.x)/((p2.x - p1.x)*(p2.x-p3.x))+
+    			p1.y*(p4.x - p3.x)*(p4.x-p2.x)/((p1.x - p3.x)*(p1.x-p2.x));
+    	return p4;
+    }
+    
+    public static Image drawCircle(Mat matrix, Point CENTER, Scalar scalar) throws Exception {
+        
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        Imgproc.circle (
+           matrix,                 //Matrix obj of the image
+           CENTER,  5,                  //Radius
+           scalar,  //Scalar object for color
+           10                      //Thickness of the circle
+        );
+      
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", matrix, matOfByte);
+
+        byte[] byteArray = matOfByte.toArray();
+
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage bufImage = ImageIO.read(in);
+   
+        return bufImage;
+     }
+    public static Point findCentroid(Mat map) {
+    	Moments moments = Imgproc.moments(map);
+    	Point centroid = new Point();
+    	centroid.x = moments.get_m10() / moments.get_m00();
+    	centroid.y = moments.get_m01() / moments.get_m00();
+    	return centroid;
+    }
+    
+    public static Image drawCurve(Mat matrix, ArrayList<Point> points) throws Exception {
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        List<MatOfPoint> list = new ArrayList();
+        List<Point> pointsL = new ArrayList();
+        int x = 1;
+        while (x < 60) {
+        	pointsL.add(points.get(points.size()-x));
+        	x++;
+        }
+        MatOfPoint MOP = new MatOfPoint ();
+        MOP.fromList(pointsL);
+        list.add(
+              MOP 
+        );
+        // Drawing polylines
+        Imgproc.polylines (
+           matrix,                    // Matrix obj of the image
+           list,                      // java.util.List<MatOfPoint> pts
+           false,                     // isClosed
+           new Scalar(200, 0, 200),     // Scalar object for color
+           8                          // Thickness of the line
+        );
+        // Encoding the image
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", matrix, matOfByte);
+
+        // Storing the encoded Mat in a byte array
+        byte[] byteArray = matOfByte.toArray();
+
+        // Displaying the image
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage bufImage = ImageIO.read(in);
+
+        return bufImage;
+     }
+
+}
